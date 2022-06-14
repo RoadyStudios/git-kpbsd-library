1d15fce8496b877f64237977fcf62ca616b45ec8
RoadyStudios <aroady@me.com>
2022-06-14 14:24:12 -0800

    Update entry_header.php

--- template-parts/content/entry_header.php
+++ template-parts/content/entry_header.php
@@ -11,12 +11,19 @@ namespace WP_Rig\WP_Rig;
 
 <header class="entry-header">
 	<?php
-	get_template_part( 'template-parts/content/entry_title', get_post_type() );
-
-	get_template_part( 'template-parts/content/entry_meta', get_post_type() );
 
 	if ( ! is_search() ) {
 		get_template_part( 'template-parts/content/entry_thumbnail', get_post_type() );
 	}
+
+	if ( ! is_front_page() && ! is_page_template() ) {
+		echo ( '<header class="entry-header">' );
+		get_template_part( 'template-parts/content/entry_title', get_post_type() );
+	}
+
+	if ( ! is_front_page() && ! is_page() && ! is_page_template() ) {
+		get_template_part( 'template-parts/content/entry_meta', get_post_type() );
+	}
+
 	?>
 </header><!-- .entry-header -->
