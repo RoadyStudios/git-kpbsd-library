4ae8018939d06e948f4aa2cff52fda01aa03b1ef
RoadyStudios <aroady@me.com>
2022-06-15 14:08:44 -0800

    cleaning up nav menu and icon coloring

--- template-parts/content/pages-sidebar.php
+++ template-parts/content/pages-sidebar.php
@@ -15,6 +15,5 @@ kpbsdlibrary()->print_styles( 'kpbsdlibrary-pages-widgets', 'kpbsdlibrary-pages-
 
 ?>
 <aside id="pages-sidebar" class="pages-sidebar widget-area" aria-label="Pages Sidebar">
-	<h3>This is testing to see if content will show up here without being inside of the call.</h3>
 	<?php kpbsdlibrary()->display_pages_sidebar(); ?>
 </aside><!-- pages-sidebar -->
