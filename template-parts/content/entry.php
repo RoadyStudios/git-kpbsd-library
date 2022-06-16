ed3b6ac5a9bb84d975f90100402d90316b6820a3
RoadyStudios <aroady@me.com>
2022-06-16 12:22:07 -0800

    fixing blog page

--- template-parts/content/entry.php
+++ template-parts/content/entry.php
@@ -13,7 +13,9 @@ namespace WP_Rig\WP_Rig;
 	<?php
 	get_template_part( 'template-parts/content/entry_header', get_post_type() );
 
-	if ( is_home() || is_search() || is_category() ) {
+	if ( is_page( 'blog' ) ) {
+		get_template_part( 'template-parts/content/entry_content', get_post_type() );
+	} elseif ( is_home() || is_search() || is_category() ) {
 		get_template_part( 'template-parts/content/entry_summary', get_post_type() );
 	} else {
 		get_template_part( 'template-parts/content/entry_content', get_post_type() );
