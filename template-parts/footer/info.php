22ba4306d8f8cd991c0f4fe2b6b7687174f09dc6
RoadyStudios <aroady@me.com>
2022-07-22 09:54:25 -0800

    still fixin 1.0.8

--- template-parts/footer/info.php
+++ template-parts/footer/info.php
@@ -12,7 +12,7 @@ namespace WP_Rig\WP_Rig;
 <div class="site-info">
 	<h2>Kenai Peninsula Borough School District</h2>
 	<div class="site-footer-logo">
-		<a href="https://kpbsd.org" title="District Home"><img src="https://www.kpbsd.k12.ak.us/images/KPBSDLogo90.png" alt="KPBSD Logo"/></a>
+		<a href="https://kpbsd.org" title="District Home" class="custom-logo-link"><img src="https://www.kpbsd.k12.ak.us/images/KPBSDLogo90.png" alt="KPBSD Logo"/></a>
 	</div>
 	<p>&copy; <?php echo date('Y'); ?> <a href="https://kpbsd.org" title="District Home">Kenai Peninsula Borough School District</a> | <a href="https://kpbsd.org/kpbsd-accessibility-statement/" title="KPBSD Accessibility Statement">Accessibility Statement</a></p>
 </div><!-- .site-info -->
